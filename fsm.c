aea0b20cda4f954a4980a69f4da0fedb374f020c
SimonPlata <124899627+SimonPlata@users.noreply.github.com>
2025-04-28 11:03:45 -0500

    Create fsm.c

---
+++ fsm.c
@@ -0,0 +1,81 @@
+/* Finite State Machine implementation template
+ *
+ * Notes:
+ * Condition evaluation priority is defined by the position in the evaluation sequence.
+ * State actions should short and not blocking. Long and complex actions should
+ * be subdivided in additional states or in a sub-machine.
+ * I/O scanning could be by demmand (every condition reads it I/O) or snapshot (All I/O
+ * read at once at the begining of the loop and freeze). The scanning method has an
+ * impact in the conditions evaluation priority.
+ *
+ * Created 25-Sep-2024
+ * by Simón Plata
+ *
+ */
+ 
+ 
+void fsm() {
+	static int state = 0;
+	
+	switch(state) {
+		state_0:	s0_io_scanning_snapshot();
+							if(state_0_condition_0) {
+								state = next_state;
+								state_0_action_0();
+							}
+							else if(state_0_condition_1) {
+								state = next_state;
+								state_0_action_1();
+							}
+							else if(state_0_condition_n) {
+								state = next_state;
+								state_0_action_n();
+							}
+							else {
+								state = same_state;
+								no_action();
+							}
+							break;
+							
+		state_1:	s1_io_scanning_snapshot();
+							if(state_1_condition_0) {
+								state = next_state;
+								state_1_action_0();
+							}
+							else if(state_1_condition_1) {
+								state = next_state;
+								state_1_action_1();
+							}
+							else if(state_1_condition_n) {
+								state = next_state;
+								state_1_action_n();
+							}
+							else {
+								state = same_state;
+								no_action();
+							}
+							break;
+							
+		state_n:	sn_io_scanning_snapshot();
+							if(state_n_condition_0) {
+								state = next_state;
+								state_1_action_0();
+							}
+							else if(state_n_condition_1) {
+								state = next_state;
+								state_1_action_1();
+							}
+							else if(state_n_condition_n) {
+								state = next_state;
+								state_1_action_n();
+							}
+							else {
+								state = same_state;
+								no_action();
+							}
+							break;
+							
+		defaul:		state = 0;
+							unknown_state_action();
+							break;
+	}
